a89a5e2752422d489c4e3e23a7fbce0319f5849d
seonb2n <626ksb@gmail.com>
2023-07-07 22:15:02 +0900

    - 큐 자료구조 학습 - 직접 구현해서 사용해야 한다.

--- dataStructure.go
+++ dataStructure.go
@@ -6,7 +6,7 @@ import (
 )
 
 // 리스트 형태
-func main() {
+func main_list() {
 	// 새로운 리스트 객체를 생성
 	l := list.New()
 	// 제일 마지막에 값을 추가
@@ -41,4 +41,42 @@ func main() {
 	//From Back
 	//4 3 2 1
 
+}
+
+// 큐 자료 구조
+// 리스트를 사용해서 구현한다.
+type Queue struct {
+	// lit 패키지의 List 타입의 포인터인 v 를 의미한다.
+	v *list.List
+}
+
+func NewQueue() *Queue {
+	return &Queue{list.New()}
+}
+
+func (q *Queue) Push(v interface{}) {
+	q.v.PushBack(v)
+}
+
+func (q *Queue) Pop() interface{} {
+	front := q.v.Front()
+	if front == nil {
+		return nil
+	}
+
+	return q.v.Remove(front)
+}
+
+func main_queue() {
+	queue := NewQueue()
+
+	for i := 0; i < 5; i++ {
+		queue.Push(i)
+	}
+
+	v := queue.Pop()
+	for v != nil {
+		fmt.Println(v)
+		v = queue.Pop()
+	}
 }
